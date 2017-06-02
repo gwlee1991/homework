9065c117fa6ab33d584c440443626504e909bff8
Andrew Lee <andrewlee@Andrews-MacBook-Air.local>
2017-06-01 23:20:30 -0700

    second draft

--- W1D4/map.rb
+++ W1D4/map.rb
@@ -1,32 +1,52 @@
 class Map
   attr_reader :map
+
   def initialize
     @map = []
   end
 
-  def assign(key, value)
+  def binary_search (array, target)
+    return nil if array.empty?
 
-
-    map.each_with_index do |keys, index|
-      if keys.first == key
-        keys[index].last = value
+    mid_index = array.length / 2
+    case array[mid_index].first <=> target
+    when -1
+      binary_search(array.take(mid_index), target)
+    when 0
+      mid_index
+    when 1
+      sub_answer = binary_search(nums.drop(mid_index + 1), target)
+      if sub_answer.nil?
+        nil
+      else
+        mid_index + sub_answer
       end
     end
+  end
 
-    map.push([key, value])
+  def assign(key, value)
+    index_of_key = binary_search(map, key)
+    (index_of_key.nil?) ? map.push([key, value]) : map[index_of_key] = [key, value]
   end
 
   def lookup(key)
     map.each do |pairs|
       if pairs.first == key
-        return "#{pairs.first} = #{pairs.last}"
+        return puts "#{pairs.first} = #{pairs.last}"
+      end
     end
 
     puts 'no match'
   end
 
   def remove(key)
+    map.each_with_index do |pairs, index|
+      if pairs.first == key
+        map[index] = nil
+      end
+    end
 
+    map.compact!
   end
 
   def show
@@ -34,3 +54,13 @@ class Map
   end
 
 end
+
+new_map = Map.new
+new_map.show
+new_map.assign('name', 'Andrew')
+new_map.show
+new_map.lookup('name')
+new_map.assign('name', 'Kelly')
+new_map.show
+new_map.remove('name')
+new_map.show
